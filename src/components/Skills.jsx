786552bb4e8bc53e4b752a2d79e1689f03143d01
maze272003 <jg.jonatas.au@phinmaed.com>
2025-10-21 21:00:23 +0800

    icons skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+// Mag-import ng specific icons mula sa library
+import {
+  FaPython, FaPhp, FaLaravel, FaJava, FaJsSquare, FaNodeJs, FaReact
+} from 'react-icons/fa';
+import { SiAndroidstudio, SiUnity, SiExpo } from 'react-icons/si';
 
 const skillsData = [
-  { name: 'Python', icon: 'https://img.icons8.com/color/48/000000/python.png' },
-  { name: 'PHP', icon: 'https://img.icons8.com/officel/48/000000/php-logo.png' },
-  { name: 'Laravel', icon: 'https://img.icons8.com/fluency/48/000000/laravel.png' },
-  { name: 'Java', icon: 'https://img.icons8.com/color/48/000000/java-coffee-cup-logo.png' },
-  { name: 'JavaScript', icon: 'https://img.icons8.com/color/48/000000/javascript.png' },
-  { name: 'Node.js', icon: 'https://img.icons8.com/color/48/000000/nodejs.png' },
-  { name: 'React Native', icon: 'https://img.icons8.com/color/48/000000/react-native.png' },
-  { name: 'Android Studio', icon: 'https://img.icons8.com/color/48/000000/android-studio--v3.png' },
-  { name: 'Unity', icon: 'https://img.icons8.com/ios-filled/50/000000/unity.png' },
-  { name: 'Expo', icon: 'https://img.icons8.com/color/48/000000/expo.png' },
+  { name: 'Python', icon: <FaPython /> },
+  { name: 'PHP', icon: <FaPhp /> },
+  { name: 'Laravel', icon: <FaLaravel /> },
+  { name: 'Java', icon: <FaJava /> },
+  { name: 'JavaScript', icon: <FaJsSquare /> },
+  { name: 'Node.js', icon: <FaNodeJs /> },
+  { name: 'React Native', icon: <FaReact /> },
+  { name: 'Android Studio', icon: <SiAndroidstudio /> },
+  { name: 'Unity', icon: <SiUnity /> },
+  { name: 'Expo', icon: <SiExpo /> },
 ];
 
 function Skills() {
@@ -23,7 +28,8 @@ function Skills() {
       <div className="skills-container">
         {skillsData.map(skill => (
           <div key={skill.name} className="skill-item">
-            <img src={skill.icon} alt={skill.name} />
+            {/* Direkta nating ilalagay ang icon component dito */}
+            {skill.icon}
             <span>{skill.name}</span>
           </div>
         ))}
