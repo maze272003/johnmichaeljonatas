ba9ea3f591decf03da98b2a91c3194d612a48497
maze272003 <jg.jonatas.au@phinmaed.com>
2025-12-02 01:09:44 +0800

    additional projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,14 +11,18 @@ import arkvisionImg from '../assets/projects/arkvsion.png';
 import autoservImg from '../assets/projects/Autoserv.png';
 import springbullbarsImg from '../assets/projects/springbullbars.png';
 import rmpoims from '../assets/projects/rctmed.png';
+import gtims from '../assets/projects/gtims.png'
+import doconnect from '../assets/projects/doconnect.png';
 
 const projectData = [
-    { id: 1, title: 'SpringBullBars - Inventory System', description: "Developed to enhance inventory management and track analytics for admins and staff.", tech: ['Vanilla PHP', 'MySQL', 'Bootstrap'], imageUrl: springbullbarsImg, liveUrl: 'https://springbullbars.shop/', status: 'Completed', statusColor: 'blue' },
-    { id: 6, title: 'RMPOIMS - Inventory System', description: 'A comprehensive inventory system that utilizes QR codes for efficient ordering and tracking.', tech: ['Laravel', 'MySQL', 'Tailwindcss'], imageUrl: rmpoims, liveUrl: 'https://rmpoims.com/', status: 'Working', statusColor: 'green' },
-    { id: 2, title: 'AutoServ Service Center', description: 'A web platform for managing car servicing appointments, tracking repairs, and customer communication.', tech: ['Laravel', 'PHP', 'MySQL'], imageUrl: autoservImg, liveUrl: '#', status: 'Expired', statusColor: 'gray' },
-    { id: 3, title: 'Lumina E-commerce', description: 'An e-commerce website offering a variety of products with a user-friendly shopping experience.', tech: ['Vanilla PHP', 'MySQL'], imageUrl: luminaImg, liveUrl: '#', status: 'Expired', statusColor: 'gray' },
-    { id: 4, title: 'Jabs Basic Sound', description: 'Offers affordable sound system rentals for events and showcases music collaborations with artists.', tech: ['HTML', 'CSS', 'JavaScript'], imageUrl: jabsImg, liveUrl: '#', status: 'Expired', statusColor: 'gray' },
-    { id: 5, title: 'ArkVision Photo Studio', description: 'A photo studio platform for professional photography services, featuring a variety of visual styles.', tech: ['Node.js', 'Express'], imageUrl: arkvisionImg, liveUrl: 'https://drive.google.com/drive/folders/16Xv620F_LgnsPllMRAUDktXM91C39U93?usp=sharing', status: 'Not Working', statusColor: 'red' },
+    { id: 1, title: 'SpringBullBars - Inventory System', description: "Developed to enhance inventory management and track analytics for admins and staff.", tech: ['Vanilla PHP', 'MySql', 'Bootstrap'], imageUrl: springbullbarsImg, liveUrl: 'https://springbullbars.shop/', status: 'Working', statusColor: 'green' },
+    { id: 6, title: 'RMPOIMS - Inventory System', description: 'A comprehensive inventory system that utilizes QR codes for efficient ordering and tracking.', tech: ['Laravel', 'MySql', 'Tailwindcss'], imageUrl: rmpoims, liveUrl: 'https://rmpoims.com/', status: 'Working', statusColor: 'green' },
+    { id: 2, title: 'AutoServ Service Center', description: 'A web platform for managing car servicing appointments, tracking repairs, and customer communication.', tech: ['Laravel', 'PHP', 'MySql'], imageUrl: autoservImg, liveUrl: '#', status: 'Expired', statusColor: 'red' },
+    { id: 3, title: 'Lumina E-commerce', description: 'An e-commerce website offering a variety of products with a user-friendly shopping experience.', tech: ['Vanilla PHP', 'MySql'], imageUrl: luminaImg, liveUrl: '#', status: 'Expired', statusColor: 'red' },
+    { id: 4, title: 'Jabs Basic Sound', description: 'Offers affordable sound system rentals for events and showcases music collaborations with artists.', tech: ['HTML', 'CSS', 'JavaScript'], imageUrl: jabsImg, liveUrl: '#', status: 'Expired', statusColor: 'red' },
+    { id: 5, title: 'ArkVision Photo Studio', description: 'A photo studio platform for professional photography services, featuring a variety of visual styles.', tech: ['Node.js', 'Express', 'mongodb'], imageUrl: arkvisionImg, liveUrl: 'https://drive.google.com/drive/folders/16Xv620F_LgnsPllMRAUDktXM91C39U93?usp=sharing', status: 'Expired', statusColor: 'red' },
+    { id: 6, title: 'General Tinio Inventory', description: 'General Tinio Inventory For hru health center.', tech: ['Laravel','MySql', 'Tailwind'], imageUrl: gtims, liveUrl: 'https://gtimss.hostcluster.site', status: 'Working', statusColor: 'green' },
+    { id: 7, title: 'Doconnect', description: 'Documents Request System For San Lorenzo Gapan City', tech: ['Laravel','MySql', 'Tailwind','React + Inertia'], imageUrl: doconnect, liveUrl: 'https://doconnect.hostcluster.site', status: 'Working', statusColor: 'green' },
 ];
 
 function Projects() {
